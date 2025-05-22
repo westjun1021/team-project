7ab9c3e0d290fbcd963a7044c119d6aa9682cdab
PARK-DEUGGYUN <emrrbs2315@gmail.com>
2025-05-22 20:10:56 +0900

    Add files via upload

---
+++ models/paper.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class Author(BaseModel):
+    order: Optional[str]
+    url:   Optional[str]
+    name:  str
+
+class Publication(BaseModel):
+    url:  Optional[str]
+    name: Optional[str]
+
+class Recommendation(BaseModel):
+    title:        Optional[str]
+    authors:      List[Author]
+    publisher:    Publication
+    publication:  Publication
+    issue_yymm:   Optional[str]
+    pages:        Optional[str]
+    free_yn:      Optional[str]
+    price:        Optional[str]
+    preview_yn:   Optional[str]
+    preview:      Optional[str]
+    link_url:     Optional[str]
+    link_api:     Optional[str]
+
+class RecommendationResponse(BaseModel):
+    totalcount:      int
+    pyymm:           Optional[str]
+    recommendations: List[Recommendation]
